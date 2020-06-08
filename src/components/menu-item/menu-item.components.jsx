b36a9cf399dbaf91828cb4bf37784dbfc857e7df
Saefullah <saefullah.kamaruddin@ucdconnect.ie>
2020-06-08 20:16:03 +0800

    shop page + shop preview

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
 // functional component used because not holding any state
 // destructure title
 // style takes an object and applies css values to an element
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -20,4 +24,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
+// with router is a higher order component. a fx that takes in a component as an argumet and modifies it
+// in the case with withRouter it adds access to location, match and history proprs
