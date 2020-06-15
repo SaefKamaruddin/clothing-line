4873ce8e8fb29bc13ddf12f765a59e738334366c
Saefullah <saefullah.kamaruddin@ucdconnect.ie>
2020-06-15 18:00:43 +0800

    added redux and selectors to shop and directory state

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,19 @@
 import React from "react";
-import SHOP_DATA from "./shop.data.jsx";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import { selectCollections } from "../../redux/shop/shop.selectors";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,28 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
+//denotes what kind of storage is used for persistence, in this case local storage is used
+//session storage can e accessed if it is imported
+import storage from "redux-persist/lib/storage";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-export default combineReducers({
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
+
+//whitelist property is an array containg any reducer name that  wants to be stored
+//so i am going to persist cart
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
